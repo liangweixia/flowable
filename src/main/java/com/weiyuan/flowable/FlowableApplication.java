2fd929dcf8aad3e11c7766eb622def799716f850
xia <1130287126@qq.com>
2019-04-15 17:15:29 +0800

    flowable init

---
+++ src/main/java/com/weiyuan/flowable/FlowableApplication.java
@@ -0,0 +1,15 @@
+package com.weiyuan.flowable;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FlowableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableApplication.class, args);
+	}
+
+}
